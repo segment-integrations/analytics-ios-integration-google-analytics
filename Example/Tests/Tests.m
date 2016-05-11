1471b5f6789f5963de8544e950d06df11e2dab9b
wcjohnson11 <williamcouperjohnson@gmail.com>
2016-05-11 15:21:24 -0700

    scaffold for remainder of tests

--- Example/Tests/Tests.m
+++ Example/Tests/Tests.m
@@ -29,4 +29,78 @@ describe(@"SEGGoogleAnalyticsIntegrationFactory", ^{
     });
 });
 
+describe(@"SEGGoogleAnalyticsIntegration", ^{
+    __block Class mockGoogleAnalytics;
+    __block SEGGoogleAnalyticsIntegration *integration;
+    
+    beforeEach(^{
+        mockGoogleAnalytics = mockClass([GAI class]);
+        integration = [[SEGGoogleAnalyticsIntegration alloc] initWithSettings:@{}];
+    });
+    
+    it(@"identify with no Traits", ^{
+        SEGIdentifyPayload *payload = [[SEGIdentifyPayload alloc] initWithUserId:@"1111" anonymousId:nil traits:@{} context:@{} integrations:@{}];
+        
+        [integration identify:payload];
+        
+        [verify(mockGoogleAnalytics) setUserAttributes: @{@"user_id": @"1111"}];
+    });
+    
+    it(@"identify with Traits", ^{
+        SEGIdentifyPayload *payload = [[SEGIdentifyPayload alloc] initWithUserId:@"1111"
+                                                                     anonymousId:nil
+                                                                          traits:@{@"name":@"Kylo Ren",
+                                                                                   @"gender": @"male",
+                                                                                   @"emotion": @"angsty"}
+                                                                         context:@{} integrations:@{}];
+        
+        [integration identify:payload];
+        
+//        [verify(mockTaplytics) setUserAttributes: @{@"user_id": @"1111",
+//                                                    @"name": @"Kylo Ren",
+//                                                    @"gender": @"male",
+//                                                    @"customData":
+//                                                        @{@"emotion":@"angsty"}
+//                                                    }];
+    });
+    
+    it(@"track with no props", ^{
+        SEGTrackPayload *payload = [[SEGTrackPayload alloc] initWithEvent:@"Starship Clicked" properties:@{} context:@{} integrations:@{}];
+        
+        [integration track:payload];
+        
+//        [verify(mockTaplytics) logEvent:@"Starship Clicked" value:nil metaData:@{}];
+    });
+    
+    it(@"track with props", ^{
+        SEGTrackPayload *payload = [[SEGTrackPayload alloc] initWithEvent:@"Starship Ordered" properties:@{@"Starship Type": @"Death Star"} context:@{} integrations:@{}];
+        
+        [integration track:payload];
+        
+//        [verify(mockTaplytics) logEvent:@"Starship Ordered" value:nil metaData:@{@"Starship Type": @"Death Star"}];
+    });
+    
+    it(@"track with revenue", ^{
+        SEGTrackPayload *payload = [[SEGTrackPayload alloc] initWithEvent:@"Starship Bought" properties:@{@"revenue": @20000} context:@{} integrations:@{}];
+        
+        [integration track:payload];
+        
+//        [verify(mockTaplytics) logRevenue:@"Starship Bought" revenue:@20000 metaData:@{}];
+    });
+    
+    it(@"track with value", ^{
+        SEGTrackPayload *payload = [[SEGTrackPayload alloc] initWithEvent:@"Starship Refueled" properties:@{@"value": @200.15, @"Fuel Type":@"Solar Cells"} context:@{} integrations:@{}];
+        
+        [integration track:payload];
+        
+//        [verify(mockTaplytics) logEvent:@"Starship Refueled" value:@200.15 metaData:@{@"Fuel Type": @"Solar Cells"}];
+    });
+    
+    it(@"reset", ^{
+        [integration reset];
+        
+//        [verify(mockTaplytics) resetUser:nil];
+    });
+});
+
 SpecEnd
