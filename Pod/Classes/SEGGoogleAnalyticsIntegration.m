3db763808969469ef820e7e590ca41e2e88abffa
ladan nasserian <ladannasserian@gmail.com>
2017-06-27 13:50:55 -0700

    Map campaign data for track calls

--- Pod/Classes/SEGGoogleAnalyticsIntegration.m
+++ Pod/Classes/SEGGoogleAnalyticsIntegration.m
@@ -115,7 +115,7 @@
                                                action:payload.event
                                                 label:label
                                                 value:value];
-    NSDictionary *hit = [self setCustomDimensionsAndMetrics:payload.properties onHit:hitBuilder];
+    NSDictionary *hit = [self setCustomDimensionsAndMetricsAndCampaignData:payload.properties context:payload.context onHit:hitBuilder];
     [self.tracker send:hit];
     SEGLog(@"[[[GAI sharedInstance] defaultTracker] send:%@];", hit);
 }
@@ -127,31 +127,8 @@
 
     GAIDictionaryBuilder *hitBuilder = [GAIDictionaryBuilder createScreenView];
     
-    NSDictionary *campaign = payload.context[@"campaign"];
-    if ([campaign isKindOfClass:[NSDictionary class]]) {
-        if ([campaign[@"source"] isKindOfClass:[NSString class]]) {
-            [hitBuilder set:campaign[@"source"] forKey:kGAICampaignSource];
-            if ([campaign[@"name"] isKindOfClass:[NSString class]]) {
-                [hitBuilder set:campaign[@"name"] forKey:kGAICampaignName];
-            }
-            if ([campaign[@"content"] isKindOfClass:[NSString class]]) {
-                [hitBuilder set:campaign[@"content"] forKey:kGAICampaignContent];
-            }
-            if ([campaign[@"medium"] isKindOfClass:[NSString class]]) {
-                [hitBuilder set:campaign[@"medium"] forKey:kGAICampaignMedium];
-            }
-        } else {
-            // https://developers.google.com/analytics/devguides/collection/ios/v3/campaigns
-            SEGLog(@"WARNING: campaign source is a required field for GA. Omitting campaign attributes");
-        }
-        // Segment does not currently spec the following keys that GA accepts
-        // kGAICampaignKeyword
-        // kGAICampaignId
-        // kGAICampaignAdNetworkClickId
-        // kGAICampaignAdNetworkId
-    }
-    
-    NSDictionary *hit = [self setCustomDimensionsAndMetrics:payload.properties onHit:hitBuilder];
+    NSDictionary *hit = [self setCustomDimensionsAndMetricsAndCampaignData:payload.properties context:payload.context onHit:hitBuilder];
+        
     [self.tracker send:hit];
     SEGLog(@"[[[GAI sharedInstance] defaultTracker] send:%@];", hit);
 }
@@ -201,7 +178,7 @@
 
 // event and screen properties are generall hit-scoped dimensions, so we want
 // to set them on the hits, not the tracker
-- (NSDictionary *)setCustomDimensionsAndMetrics:(NSDictionary *)properties onHit:(GAIDictionaryBuilder *)hit
+- (NSDictionary *)setCustomDimensionsAndMetricsAndCampaignData:(NSDictionary *)properties context:(NSDictionary *)context onHit:(GAIDictionaryBuilder *)hit
 {
     NSDictionary *customDimensions = self.settings[@"dimensions"];
     NSDictionary *customMetrics = self.settings[@"metrics"];
@@ -221,9 +198,34 @@
                 forKey:[GAIFields customMetricForIndex:metric]];
         }
     }
+    
+    NSDictionary *campaign = context[@"campaign"];
+    if ([campaign isKindOfClass:[NSDictionary class]]) {
+        if ([campaign[@"source"] isKindOfClass:[NSString class]]) {
+            [hit set:campaign[@"source"] forKey:kGAICampaignSource];
+        if ([campaign[@"name"] isKindOfClass:[NSString class]]) {
+            [hit set:campaign[@"name"] forKey:kGAICampaignName];
+        }
+        if ([campaign[@"content"] isKindOfClass:[NSString class]]) {
+            [hit set:campaign[@"content"] forKey:kGAICampaignContent];
+        }
+        if ([campaign[@"medium"] isKindOfClass:[NSString class]]) {
+            [hit set:campaign[@"medium"] forKey:kGAICampaignMedium];
+        }
+    } else {
+            // https://developers.google.com/analytics/devguides/collection/ios/v3/campaigns
+            SEGLog(@"WARNING: campaign source is a required field for GA. Omitting campaign attributes");
+        }
+        // Segment does not currently spec the following keys that GA accepts
+        // kGAICampaignKeyword
+        // kGAICampaignId
+        // kGAICampaignAdNetworkClickId
+        // kGAICampaignAdNetworkId
+    }
 
     return [hit build];
 }
+
 
 // e.g. extractNumber:"dimension3" from:[@"dimension" length] returns 3
 // e.g. extractNumber:"metric9" from:[@"metric" length] returns 9
